6ae4b83ae7831f8618384df568afaeda9cf71d78
kai <sea.k.naka@gmail.com>
2022-09-19 16:24:01 +0900

    vue-install

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+
+import { createApp, ref } from 'vue/dist/vue.esm-bundler';
+
+createApp({
+    setup() {
+        const greeting = ref('ok');
+
+        return {
+            greeting,
+        }
+    },
+}).mount('#app');
